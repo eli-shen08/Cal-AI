e3050a042641480e4bc1823851eae50eecc02668
Rahul Mondal <mondal.rahul1999@gmail.com>
2026-05-06 22:11:57 +0530

    Vector code changed first use semantic then recursive

--- vector.py
+++ vector.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_experimental.text_splitter import SemanticChunker
+# from chunking_evaluation.chunking import ClusterSemanticChunker
 from langchain_ollama import OllamaEmbeddings
 from langchain_core.documents import Document
 from langchain_chroma import Chroma
@@ -46,23 +47,20 @@ if vectorize:
     """
 
     # First using Recursive Chunking to match context size of Embed Model
-    print("Now using Recursive text Splitter")
-    size_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1024,      
-    chunk_overlap=80     
-    )
-    rec_documents = size_splitter.split_documents(cleaned_docs)
-    print("Number of Chunks created : ", len(rec_documents))
-
-
-
     # Splitting the output of Recursive Chunking into semantic chunks
     print("Now using Semantic Chunker")
     text_splitter = SemanticChunker(embed_model)
-    sem_documents = text_splitter.split_documents(rec_documents)
+    sem_documents = text_splitter.split_documents(cleaned_docs)
     print("Number of chunks created: ", len(sem_documents))
 
 
+    print("Now using Recursive text Splitter")
+    size_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1024,      
+    chunk_overlap=50    
+    )
+    rec_documents = size_splitter.split_documents(sem_documents)
+    print("Number of Chunks created : ", len(rec_documents))
 
     vector_store = Chroma.from_documents(
         documents=rec_documents,
